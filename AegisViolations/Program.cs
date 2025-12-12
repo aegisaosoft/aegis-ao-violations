236f52ad3069a13e7fdc556b7b5be4fb896aa938
aegisaosoft <aegisaosoft@gmail.com>
2025-12-12 06:41:43 -0500

    swagger

--- AegisViolations/Program.cs
+++ AegisViolations/Program.cs
@@ -3,7 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Aegis Violations API",
+        Version = "v1",
+        Description = "API for managing violations"
+    });
+});
 
 // Configure CORS
 builder.Services.AddCors(options =>
@@ -23,12 +31,15 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Make Swagger available in all environments
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aegis Violations API v1");
+    c.RoutePrefix = "swagger"; // Swagger UI will be available at /swagger
+    c.DisplayRequestDuration();
+});
+app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
